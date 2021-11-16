c315b4a3aac8ed5d8137778beb9f22ed98f94eb2
Shadman Shahriar <shadman.shahriar@dsinnovators.com>
2021-11-16 17:57:32 +0600

    api test added

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,14 @@
 describe('cypress running test', () => {
     it('renders',  () => {
-        cy.visit('/');
+        cy.request('/').then(res => {
+            expect(res.status).to.eq(200);
+            expect(res.body._links.todos.templated).to.eq(true);
+            cy.writeFile('cypress/fixtures/base-url.json', res);
+        });
+        cy.request('/todos').then(res => {
+            expect(res.status).to.eq(200);
+            expect(res.body[0].name).to.eq('Shadman');
+            cy.writeFile('cypress/fixtures/todos.json', res);
+        });
     });
 })
