dba2467739da3c49569b513effcb667c11945856
Shadman Shahriar <shadman.shahriar@dsinnovators.com>
2021-11-24 12:58:36 +0600

    modified for next build

--- components/todos/[id].js
+++ components/todos/[id].js
@@ -1,5 +1,5 @@
 import Cors from 'cors';
-import initMiddleware from "../../../lib/init-middleware";
+import initMiddleware from "../../lib/init-middleware";
 import axios from "axios";
 
 // Initialize the cors middleware
